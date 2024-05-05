ae4933bf9c845d4054ef13023513818f9deacc42
Santos Juvenal Quispe Tapia <squispeta@compartamos.pe>
2024-05-04 19:58:14 -0500

    [React v27] Crear TODOs: React Context dentro de React Portals

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,20 +1,44 @@
 import React from "react";
 import './TodoForm.css';
+import { TodoConext } from '../TodoContext'
 
 function TodoForm(){
+
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext(TodoConext)
+    const [newTodoValue, setNewTodoValue] = React.useState('')
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    const onCancel = () => {
+        setOpenModal(false);
+    };
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
+
     return (
-        <form onSubmit={(event) => {
-            event.preventDefault();
-        }}>
+        <form onSubmit={ onSubmit }>
             <label>Escribe tu nuevo TODO</label>
             <textarea
                 placeholder="Cortar cebolla para el almuerrzo"
+                value={newTodoValue}
+                onChange={onChange}
+                required
             />
             <div className="TodoForm-buttonContainer">
                 <button
-                    type=""
+                    type="button"
                     className="TodoForm-button
                     TodoForm-button--cancel"
+                    onClick={onCancel}
                 >Cancelar</button>
                 <button
                     type="submit"
